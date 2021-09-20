6da3278b7b0f079e97c1d3c708bffddf8320794d
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2021-09-20 14:53:45 -0300

    CategoryResource

---
+++ src/main/java/com/dscatalog/entities/Category.java
@@ -0,0 +1,56 @@
+package com.dscatalog.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	
+	public Category() {
+	}
+
+	public Category(Long id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
