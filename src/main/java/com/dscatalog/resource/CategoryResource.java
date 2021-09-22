f1f7621aa75caab18a5fb1e2cc37fa9e82786b64
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2021-09-22 15:09:22 -0300

    Busca por ID

--- src/main/java/com/dscatalog/resource/CategoryResource.java
+++ src/main/java/com/dscatalog/resource/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,9 @@ public class CategoryResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findId(@PathVariable Long id){
+	    CategoryDTO dto = service.findId(id);	
+		return ResponseEntity.ok().body(dto);
+	}
 }
--- src/main/java/com/dscatalog/services/CategoryService.java
+++ src/main/java/com/dscatalog/services/CategoryService.java
@@ -1,7 +1,7 @@
 package com.dscatalog.services;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +24,13 @@ public class CategoryService {
 	  
 	  return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	  
-	  
-//	  List<CategoryDTO> listDto = new ArrayList<>();
-//	  for(Category cat : list) {
-//		  listDto.add(new CategoryDTO(cat));
-//	  }
-//	  return listDto;
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findId(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
+
 	}
 }
