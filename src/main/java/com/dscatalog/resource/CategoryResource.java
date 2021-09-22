9a39f8e1b6a0ddedf54fa68682dbe4d6fb637518
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2021-09-22 10:35:25 -0300

    DTO

--- src/main/java/com/dscatalog/resource/CategoryResource.java
+++ src/main/java/com/dscatalog/resource/CategoryResource.java
@@ -1,6 +1,5 @@
 package com.dscatalog.resource;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dscatalog.entities.Category;
+import com.dscatalog.dto.CategoryDTO;
 import com.dscatalog.services.CategoryService;
 
 @RestController
@@ -20,8 +19,8 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-	    List<Category> list = service.findAll();	
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+	    List<CategoryDTO> list = service.findAll();	
 		return ResponseEntity.ok().body(list);
 	}
 	
--- src/main/java/com/dscatalog/services/CategoryService.java
+++ src/main/java/com/dscatalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dscatalog.dto.CategoryDTO;
 import com.dscatalog.entities.Category;
 import com.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +19,16 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-	  return repository.findAll();	
+	public List<CategoryDTO> findAll(){
+	  List<Category> list = repository.findAll();	
+	  
+	  return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	  
+	  
+//	  List<CategoryDTO> listDto = new ArrayList<>();
+//	  for(Category cat : list) {
+//		  listDto.add(new CategoryDTO(cat));
+//	  }
+//	  return listDto;
 	}
 }
