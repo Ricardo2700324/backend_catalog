bf30314fd2ab5feedd3d4023119b9c40ee01c055
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2021-12-04 13:44:15 -0300

    Created entities Role and User

--- src/main/java/com/dscatalog/entities/Category.java
+++ src/main/java/com/dscatalog/entities/Category.java
@@ -2,13 +2,16 @@ package com.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -29,6 +32,10 @@ public class Category implements Serializable {
 
 	@Column(columnDefinition = "TimeStamp without Time Zone")
 	private Instant createdAt;
+	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>() ;
+	
 	
 	public Category() {
 	}
@@ -71,6 +78,10 @@ public class Category implements Serializable {
 	@PreUpdate
 	public void preUpdate() {
 		updateAt = Instant.now();
+	}
+	
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
